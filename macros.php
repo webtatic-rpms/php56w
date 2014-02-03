f60046d902865aecbaad79b846437b1bdd8c5356
Andy Thompson <andy@webtatic.com>
2014-02-03 19:47:07 +0000

    Update from el7 php package.

--- macros.php
+++ macros.php
@@ -3,9 +3,10 @@
 # 
 %php_core_api @PHP_APIVER@
 %php_zend_api @PHP_ZENDVER@
-%php_pdo_api @PHP_PDOVER@
+%php_pdo_api  @PHP_PDOVER@
+%php_version  @PHP_VERSION@
 
-%php_extdir %{_libdir}/php/modules
+%php_extdir    %{_libdir}/php/modules
 %php_ztsextdir %{_libdir}/php-zts/modules
 
 %php_inidir    %{_sysconfdir}/php.d
